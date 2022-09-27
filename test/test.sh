5fc31a7ca562fd8af4eb0110229dc974f2a998be
Guillaume <92152391+GlaceCoding@users.noreply.github.com>
2022-09-27 22:31:54 +0200

    Ebauche du répertoire test et de la Github action correspondante

---
+++ test/test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+make -C ..
+if [[ $? == 0 ]]; then
+	echo "se compile :)"
+	make run -C ..
+else
+	echo "error compilation :("
+	exit 1
+fi
+
+if [[ $? == 0 ]]; then
+	echo "s'execute :)"
+else
+	echo "error execution :("
+	exit 1
+fi
