7932e2564c5256121573386ef17676831e785648
Guillaume <92152391+GlaceCoding@users.noreply.github.com>
2022-12-02 15:11:17 +0100

    [CODE CHANGE] Change regex to work if an other cookie exist after

--- demo/www/cgi-bin/image.pl
+++ demo/www/cgi-bin/image.pl
@@ -2,7 +2,7 @@
 
 # Example: Send a full custom header
 print "HTTP/1.1 302 Found\r\n";
-if ($ENV{'COOKIE'} =~ m/:[0-9]{2,4}\/upload\/([A-Z0-9_.-]+\.(?:png|jpg|jpeg))$/i && -s "../upload/$1")
+if ($ENV{'COOKIE'} =~ m/:[0-9]{2,4}\/upload\/([A-Z0-9_.-]+\.(?:png|jpg|jpeg))(;|$)/i && -s "../upload/$1")
 {
 	print "Location: /upload/$1\r\n";
 }
